77e941fca280e2466d6d61c00af6931170ec5bc8
Vangara Sandeep Kumar <101511412+vangara-sandeep@users.noreply.github.com>
2024-12-17 01:16:49 +0530

    Create MessagingAppApplication.java

---
+++ backend/src/main/java/com/example/messagingapp/MessagingAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.messagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingAppApplication.class, args);
+    }
+}
