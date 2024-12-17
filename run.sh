6ece23edf363376bbfd10fc920cb00f641115222
Vangara Sandeep Kumar <101511412+vangara-sandeep@users.noreply.github.com>
2024-12-18 02:13:31 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
-#!/bin/bash
-
-# Set Tomcat directory path
-CATALINA_HOME="/usr/share/tomcat9"
-TOMCAT_WEBAPPS="$CATALINA_HOME/webapps"
-
-# Step 1: Build and deploy your application
-# For this example, copy your project files to the Tomcat webapps folder.
-cp -r /home/ubuntu/your-repository /usr/share/tomcat9/webapps/vtalent
-
-# Step 2: Restart Tomcat to deploy the new app
-sudo systemctl restart tomcat9
-
-# Step 3: Expose the site on port 8080
-echo "VTalent website is now running on http://<your-ec2-public-ip>:8080/vtalent"
+sudo apt upgrade -y && sudo apt update -y
+sudo apt install apache2 -y
+sudo rm -rf /var/www/html/*
+sudo cp * /var/www/html
+sudo chmod 666 welcome.html && sudo chmod 666 index.html
